e577695e551c601e38e273448a2a5e06e5be867d
rishu <rrishu212@gmail.com>
2024-09-11 21:33:39 -0700

    Dynamic Routes ||

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,4 +1,5 @@
 const Product = require('../models/product');
+const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
@@ -9,6 +10,17 @@ exports.getProducts = (req, res, next) => {
     });
   });
 };
+
+exports.getProduct = (req,res,next) => {
+  const prodId = req.params.productId;
+  Product.findById(prodId, product => {
+    res.render('shop/product-detail', {
+      product: product,
+      pageTitle: product.title,
+      path: '/products'
+    })
+  })
+}
 
 exports.getIndex = (req, res, next) => {
   Product.fetchAll(products => {
@@ -26,6 +38,14 @@ exports.getCart = (req, res, next) => {
     pageTitle: 'Your Cart'
   });
 };
+
+exports.postCart = (req,res,next) => {
+   const prodId = req.body.productId;
+   Product.findById(prodId, (product) => {
+    Cart.addProduct(prodId, product.price);
+   })
+   res.redirect('/cart');
+}
 
 exports.getOrders = (req, res, next) => {
   res.render('shop/orders', {
